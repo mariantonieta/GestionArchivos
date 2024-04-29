3df0a70ffe4700157df489931ea11853259ccc97
mohamed <mohamedhadjkacem4@gmail.com>
2024-04-29 15:51:49 +0200

    commit2

--- ArchivoVentas.java
+++ ArchivoVentas.java
@@ -1,8 +1,8 @@
 import java.io.*;
-import java.sql.Date;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-
+/*
 public class ArchivoVentas extends Archivo{
     List<String> registro = new ArrayList<>();
 
@@ -30,8 +30,8 @@ public class ArchivoVentas extends Archivo{
                 String[] datosVenta = linea.split(",");
                 if (datosVenta.length >= 4) {
                     Date fecha = Date.valueOf(datosVenta[0].trim());
-                    String cliente = datosVenta[1].trim();
-                    String librosVendidos = datosVenta[2].trim();
+                    Cliente cliente = datosVenta[1].trim();
+                    Libro librosVendidos = datosVenta[2].trim();
                     Double total = Double.valueOf(datosVenta[3].trim());
 
                     Venta Venta = new Venta(fecha, cliente, librosVendidos,total);
@@ -46,4 +46,67 @@ public class ArchivoVentas extends Archivo{
         }
         return ventas ;
     }
+}*/
+
+public class ArchivoVentas extends Archivo {
+
+    private List<String> registro = new ArrayList<>();
+    private Object mapper;
+
+
+    public void guardarVentas(List<Venta> ventas) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(super.getRuta()))) {
+            bw.write("transaccion,");
+            bw.write("fecha,cliente,librosVendidos,total,idVenta,descuento,precio,fechaEntrega\n");
+
+            for (Venta venta : ventas) {
+                String datos = venta.getFechaVenta().toString() + "," + venta.getClientes().toString() + "," + venta.getLibrosVendidos().toString() + "," + venta.calcularTotal() + "," + venta.getIdVenta()+","+venta.getDescuento()+","+venta.getPrecio()+","+venta.getFechaEntrega();
+                bw.write(datos);
+                bw.newLine();
+                registro.add(datos);
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error al guardar ventas: " + e.getMessage());
+        }
+    }
+
+
+
+    public List<Venta> cargarVentas() {
+        List<Venta> ventas = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(super.getRuta()))) {
+            br.readLine();
+            String linea;
+            List <Libro> libroVendidos = new ArrayList<>();
+            while ((linea = br.readLine()) != null) {
+                String[] datosVenta = linea.split(",");
+                if (datosVenta.length >= 8) {
+                    LocalDate fechaVenta = LocalDate.parse(datosVenta[0].trim());
+                    List<Cliente> clientes = mapper.datosVenta(datosVenta[1].trim());
+                    String librosVendidos = datosVenta[2].trim();
+                    Double total = Double.valueOf(datosVenta[3].trim());
+                    int idVenta = Integer.parseInt(datosVenta[4].trim());
+                    Double descuento = Double.valueOf(datosVenta[5].trim());
+                    Double precio = Double.valueOf(datosVenta[6].trim());
+                    String fechaEntregaString = datosVenta[7].trim();
+                    LocalDate fechaEntrega = LocalDate.parse(fechaEntregaString);
+
+                    //Venta venta= new Venta( fechaVenta,  clientes,  librosVendidos,  total, idVenta,descuento,precio,fechaEntrega);
+
+                    Venta venta= new Venta(null,  fechaVenta,  libroVendidos,  clientes);
+                    ventas.add(venta);
+                } else {
+                    System.out.println("No hay mas ventas");
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error al cargar ventas: " + e.getMessage());
+        }
+        return ventas;
+    }
+
 }
+
